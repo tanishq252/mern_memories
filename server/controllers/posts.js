d699fb01d47736f8bd8ac93fe9fe41ec95748f47
Tanishq Deshpande <tanishq252002@gmail.com>
2022-05-26 21:15:13 +0530

    CRUD operations successfully completed

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -26,14 +26,40 @@ export const createPost = async (req, res) => {
 
 // update
 export const updatePost = async (req, res) => {
-
     const {id: _id} = req.params;
     const post = req.body;
 
     if(!mongoose.Types.ObjectId.isValid(_id)) res.status(404).json({message: 'No post with given id exists'}); 
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {new:true});
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id} , {new:true});
 
     res.json(updatePost);
+}
 
+// delete
+export const deletePost = async(req, res) => {
+    const {id} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) res.status(404).json({message: 'No post with given id exists'})
+
+    try{
+        await PostMessage.findByIdAndRemove(id);
+        res.status(200).json({message: "successfully deleted!"});
+
+    }catch(e){
+        res.status(404).json({message: e});
+
+    }
+}
+
+// like
+export const likePost = async (req, res) => {
+    const {id} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) res.status(404).json({message: 'No post with given id exists'}); 
+
+    const post = await PostMessage.findById(id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, {likeCount: post.likeCount+1} , {new:true})
+
+    res.json(updatedPost);
 }
